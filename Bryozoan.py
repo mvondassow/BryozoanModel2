a15e95ffc04fe2def8a79f0a59a6bdf0a6d302cf
mvondassow <mvondass@gmail.com>
2017-02-14 21:28:19 -0800

    Various updates and corrections.
    Mostly updated BryoTheory to clarify rationale for form of dC/dt used.

--- Bryozoan.py
+++ Bryozoan.py
@@ -120,8 +120,8 @@ def dCdt_default(Cs, dPs, params):
     dPs : array, dim = 1
         1-by-n array of pressure differences (floats; same length as Cs)
     params : dictionary
-        params must contain keys 'w', 'yminusx', 'r', and 'b' for parameters
-        (all numeric types).
+        params must contain keys 'w', 'yminusx', 'r', 'b', 'c0' 
+        for parameters (all numeric types).
 
     Returns
     -------
@@ -131,30 +131,29 @@ def dCdt_default(Cs, dPs, params):
 
     Justification for form of S & dC/dt:
     ------------------------------------
-    shearStress*perimeter*length = pressureDrop*crosssectionArea
+    shear*perimeter*length = pressureDrop*crosssectionArea
 
     h = parameter with units of length (radius, width, height...)
         describing conduit width
 
-    perimeter ~ A*h^x, 0 ≤ x ≤ 1 (A is constant of proportionality;
-        x = 0 if h is separation between infinite parallel plates;
-        x = 1 if h is radius or width of conduit that scales
-        isotropically, e.g. radius of cylindrical pipe).
+    perimeter ~ A*h^x, 0 ≤ x ≤ 1 (x = 0 if h is separation between infinite
+        parallel plates; x = 1 if h is radius or width of conduit that
+        scales isotropically, e.g. radius of cylindrical pipe).
     crosssectionArea ~ K*h^y, 1 ≤ y ≤ 2 (y = 1 if h is separation between
         infinite parallel plates; y = 2 if h is radius or width of conduit
         that scales isotropically (e.g. radius of cylindrical pipe);
         BUT, for height of vertical parallel plates, area and perimeter
         both x and y increase in direct proporion to height: y = x = 1.
     Therefore, assuming conduit length is constant:
-    shearStress ~ pressureDrop*a*h^(y-x) : 0 ≤ (y-x) ≤ 1 (with a = K/(A*length)).
+    shear ~ pressureDrop*a*h^(y-x) : 0 ≤ (y-x) ≤ 1 (with a = K/(A*length)).
 
     conductivity ~ d*h^w, 1 ≤ w ≤ 4 (w = 3 if h is separation of plates;
         w = 4 if h is radius of cylindrical pipe; w = 1 if h is height of
-        parallel vertical plates, assuming their separation is much smaller
-        than their height; d is a constant).
+        parallel vertical plates (assuming their separation is much smaller
+        than their height)).
 
     Hence: h ~ (conductivity/d)^(1/w) and:
-    shearStress ~ pressureDrop*(a/(d^z))*conductivity^z; z=(y-x)/w, b=a/(d^z)
+    shear ~ pressureDrop*(a/(d^z))*conductivity^z; z=(y-x)/w, b=a/(d^z)
 
     For the three cases above:
         separation between parallel plates: x = 0, y = 1, w = 3 : z = 1/3
@@ -164,19 +163,20 @@ def dCdt_default(Cs, dPs, params):
     Therefore, set S = b*(conductivity^z)*pressureDrop, 0 ≤ z ≤ 1/3
     As matrices (conductivity as diagonal matrix)
         S = b*abs(sum(conductivity[i,j]^z*sum(Incidence[j,k]*Pressures[k])
-        
-    Assuming dh/dt=r*(S-s0) (r is a constant), dC/dt = (d*h^(w-1))*dh/dt
-    hence: dC/dt = d*(C/d)^((w-1)/w)*r*(S-s0) 
-    dC/dt = d^((2w-1)/w) * C^((w-1)/w))*r*(S-s0) 
-    One can choose parameters such that:
-    dC/dt = r2*(C^((w-1)/w))*(S-1)
+
+    Assuming dh/dt=r1*(S-s0), dC/dt = (d*h^(w-1))*dh/dt
+    dC/dt = d*(C/d)^((w-1)/w)*r*(S-s0)
+    dC/dt = d^((2w-1)/w) * C^((w-1)/w))*r(S-s0) one can choose parameters so:
+    dC/dt = r*(C^q)*(S-1) with q = (w-1)/w so 0<q<3/4
     """
     w = params.get('w')
     z = params.get('yminusx')/w
-    # Added line to set floor on conductivities.
-    Cs[Cs < 0] = 0
-    S = abs(params.get('b')*(Cs**z)*dPs)
-    dCdt = params.get('r') * (Cs**((w-1)/w)) * (S - 1)
+    # Floor on conductivities set to 0.
+    Cflr = np.maximum(Cs, 0)
+    S = abs(params.get('b')*(Cflr**z)*dPs)
+    dCdt = params.get('r') * (Cflr**((w-1)/w)) * (S - 1)
+    # For Cs <= c0, only allow positive dC/dt
+    dCdt[(Cflr < params.get('c0')) & (dCdt < 0)] = 0
     return dCdt, S
 
 
@@ -691,8 +691,10 @@ if __name__ == '__main__':
     if demo == 'y':
         # Create colony object.
         c1 = Colony(nz=6, mz=7, OutflowConductivity=0.01, dCdt=dCdt_default,
-                    dCdt_in_params={'yminusx': 1, 'b': 1, 'r': 1, 'w': 3},
-                    dCdt_out_params={'yminusx': 1, 'b': 0.1, 'r': 1, 'w': 3})
+                    dCdt_in_params={'yminusx': 1, 'b': 3, 'r': 0.2, 'w': 3,
+                                    'c0': 0.5},
+                    dCdt_out_params={'yminusx': 1, 'b': 0.3, 'r': 1, 'w': 3,
+                                     'c0': 0.0009})
         # Set a central outflow conduit (edge) to have higher conductivity
         c1.setouterconductivities([41], [0.02])
         # Create plot
